3bb2d1f4f63ef8bb0d9df2969aa22c72839b0803
RSlysz <remi@unity3d.com>
2018-09-17 15:49:14 +0200

    Remove ProbeWrapper
    HDProbe is already a wrapper between Planar and standard (HD) probes

--- com.unity.render-pipelines.high-definition/Runtime/Lighting/Reflection/PlanarReflectionProbe.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Lighting/Reflection/PlanarReflectionProbe.cs
@@ -31,10 +31,6 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
         [SerializeField]
         Vector3 m_CaptureLocalPosition;
         [SerializeField]
-        Texture m_CustomTexture;
-        [SerializeField]
-        Texture m_BakedTexture;
-        [SerializeField]
         CapturePositionMode m_CapturePositionMode = CapturePositionMode.Static;
         [SerializeField]
         Vector3 m_CaptureMirrorPlaneLocalPosition;
@@ -56,40 +52,9 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
         public float fieldOfViewOverride { get { return m_FieldOfViewOverride; } }
 
         public BoundingSphere boundingSphere { get { return influenceVolume.GetBoundingSphereAt(transform); } }
-
-        public Texture texture
-        {
-            get
-            {
-                switch (mode)
-                {
-                    default:
-                    case ReflectionProbeMode.Baked:
-                        return bakedTexture;
-                    case ReflectionProbeMode.Custom:
-                        return customTexture;
-                    case ReflectionProbeMode.Realtime:
-                        return realtimeTexture;
-                }
-            }
-        }
         public Bounds bounds { get { return influenceVolume.GetBoundsAt(transform); } }
         public Vector3 captureLocalPosition { get { return m_CaptureLocalPosition; } set { m_CaptureLocalPosition = value; } }
-        public Matrix4x4 influenceToWorld
-        {
-            get
-            {
-                var tr = transform;
-                var influencePosition = influenceVolume.GetWorldPosition(tr);
-                return Matrix4x4.TRS(
-                    influencePosition,
-                    tr.rotation,
-                    Vector3.one
-                    );
-            }
-        }
-        public Texture customTexture { get { return m_CustomTexture; } set { m_CustomTexture = value; } }
-        public Texture bakedTexture { get { return m_BakedTexture; } set { m_BakedTexture = value; }}
+
         public float captureNearPlane { get { return m_CaptureNearPlane; } }
         public float captureFarPlane { get { return m_CaptureFarPlane; } }
         public CapturePositionMode capturePositionMode { get { return m_CapturePositionMode; } }
@@ -105,36 +70,7 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
             set { m_CaptureMirrorPlaneLocalNormal = value; }
         }
         public Vector3 captureMirrorPlaneNormal { get { return transform.TransformDirection(m_CaptureMirrorPlaneLocalNormal); } }
-
-        #region Proxy Properties
-        public Matrix4x4 proxyToWorld
-        {
-            get
-            {
-                return proxyVolume != null
-                    ? Matrix4x4.TRS(proxyVolume.transform.position, proxyVolume.transform.rotation, Vector3.one)
-                    : influenceToWorld;
-            }
-        }
-        public ProxyShape proxyShape
-        {
-            get
-            {
-                return proxyVolume != null
-                    ? proxyVolume.proxyVolume.shape
-                    : (ProxyShape)influenceVolume.shape;
-            }
-        }
-        public Vector3 proxyExtents
-        {
-            get
-            {
-                return proxyVolume != null
-                    ? proxyVolume.proxyVolume.extents
-                    : influenceVolume.boxSize;
-            }
-        }
-
+        
         public bool useMirrorPlane
         {
             get
@@ -144,8 +80,6 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
                     && capturePositionMode == CapturePositionMode.MirrorCamera;
             }
         }
-
-        #endregion
 
         public void RequestRealtimeRender()
         {
