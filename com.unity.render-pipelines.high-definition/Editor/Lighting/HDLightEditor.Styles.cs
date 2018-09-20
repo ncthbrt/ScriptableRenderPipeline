4d7a34fa131e1d94700807a827627512d92d736d
Evgenii Golubev <evgenii@unity3d.com>
2018-09-20 11:52:03 +0200

    Improve naming

--- com.unity.render-pipelines.high-definition/Editor/Lighting/HDLightEditor.Styles.cs
+++ com.unity.render-pipelines.high-definition/Editor/Lighting/HDLightEditor.Styles.cs
@@ -45,8 +45,8 @@ namespace UnityEditor.Experimental.Rendering.HDPipeline
             public readonly GUIContent displayAreaLightEmissiveMesh = new GUIContent("Display Emissive Mesh", "Generate an emissive mesh using the size, color and intensity of the area light");
             public readonly GUIContent lightLayer = new GUIContent("Light Layer", "Specifies the current light layers that the light affect. Corresponding renderer with the same flags will be lit by this light.");
 
-            public readonly GUIContent sunDiskSize = new GUIContent("Sun Disk Size", "Controls the size of the highlight of the sun disk. It's the angle of the sun cone in degrees.");
-            public readonly GUIContent sunHaloPercent = new GUIContent("Sun Halo Percent", "Controls the size of the halo around the highlight of the sun disk.");
+            public readonly GUIContent sunDiskSize = new GUIContent("Sun Highlight Disk Size", "Controls the size of the highlight of the sun disk. It's the angle of the sun cone in degrees.");
+            public readonly GUIContent sunHaloPercent = new GUIContent("Sun Highlight Halo Percent", "Controls the size of the halo around the highlight of the sun disk.");
 
             public readonly GUIContent shape = new GUIContent("Type", "Specifies the current type of light. Possible types are Directional, Spot, Point, Rectangle and Line lights.");
             public readonly GUIContent[] shapeNames;
