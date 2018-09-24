317c1352550421347f7f48cb75da9aea5d41d9fa
Felipe Lira <felipedrl@gmail.com>
2018-09-24 18:41:24 +0200

    Fixed issue that caused full screen mesh to be reallocated everytime

--- com.unity.render-pipelines.lightweight/Runtime/ScriptableRenderer.cs
+++ com.unity.render-pipelines.lightweight/Runtime/ScriptableRenderer.cs
@@ -73,8 +73,8 @@ namespace UnityEngine.Experimental.Rendering.LightweightRP
                 float topV = 1.0f;
                 float bottomV = 0.0f;
 
-                Mesh mesh = new Mesh { name = "Fullscreen Quad" };
-                mesh.SetVertices(new List<Vector3>
+                s_FullscreenMesh = new Mesh { name = "Fullscreen Quad" };
+                s_FullscreenMesh.SetVertices(new List<Vector3>
                 {
                     new Vector3(-1.0f, -1.0f, 0.0f),
                     new Vector3(-1.0f,  1.0f, 0.0f),
@@ -82,7 +82,7 @@ namespace UnityEngine.Experimental.Rendering.LightweightRP
                     new Vector3(1.0f,  1.0f, 0.0f)
                 });
 
-                mesh.SetUVs(0, new List<Vector2>
+                s_FullscreenMesh.SetUVs(0, new List<Vector2>
                 {
                     new Vector2(0.0f, bottomV),
                     new Vector2(0.0f, topV),
@@ -90,9 +90,9 @@ namespace UnityEngine.Experimental.Rendering.LightweightRP
                     new Vector2(1.0f, topV)
                 });
 
-                mesh.SetIndices(new[] { 0, 1, 2, 2, 1, 3 }, MeshTopology.Triangles, 0, false);
-                mesh.UploadMeshData(true);
-                return mesh;
+                s_FullscreenMesh.SetIndices(new[] { 0, 1, 2, 2, 1, 3 }, MeshTopology.Triangles, 0, false);
+                s_FullscreenMesh.UploadMeshData(true);
+                return s_FullscreenMesh;
             }
         }
 
