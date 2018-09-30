f96d2e9d9c78afecf86e0d63d800a1b57021a396
Nick Cuthbert <nick@cuthbert.co.za>
2018-09-30 17:06:17 +0200

    Got watercolor shader ported over

--- TestProjects/LWGraphicsTest/Assets/Scripts/WaterColorFilter.shader
+++ TestProjects/LWGraphicsTest/Assets/Scripts/WaterColorFilter.shader
@@ -3,78 +3,16 @@
 
         #include "PostProcessing/Shaders/StdLib.hlsl"
 
-        TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
-        TEXTURE2D_SAMPLER2D(_WobbTex, sampler_WobbTex);
-        TEXTURE2D_SAMPLER2D(_PaperTex, sampler_PaperTex);
-        
+        TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);        
 		uniform float _WobbScale;
         uniform float _WobbPower;
         uniform float _EdgeSize;
         uniform float _EdgePower;        
-        uniform float _PaperScale;
-        uniform float _PaperPower;
         uniform float4 _MainTex_TexelSize;
 
         float4 ColorMod(float4 c, float d) {
             return (c - (c - c * c) * (d - 1));
-        }
-        
-        float4 Frag(VaryingsDefault i) : SV_Target
-        {
-            float4 color = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord);
-            float luminance = dot(color.rgb, float3(0.2126729, 0.7151522, 0.0721750));
-            color.rgb = luminance.xxx;
-            return color;
-        }
-
-        
-        float4 FragDefault(VaryingsDefault i) : SV_Target
-        {
-            float4 color = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord);            
-            float luminance = dot(color.rgb, float3(0.2126729, 0.7151522, 0.0721750));
-            return luminance.xxxx;
-        }
-
-        float4 FragEdgeDarkening(VaryingsDefault i): SV_Target {
-            float2 uv_offset = _EdgeSize * _MainTex_TexelSize.xy;
-            float4 src_l = SAMPLE_TEXTURE2D(_MainTex,sampler_MainTex, i.texcoord + float2(-uv_offset.x, 0));
-            float4 src_r = SAMPLE_TEXTURE2D(_MainTex,sampler_MainTex, i.texcoord + float2(+uv_offset.x, 0));
-            float4 src_b = SAMPLE_TEXTURE2D(_MainTex,sampler_MainTex, i.texcoord + float2(0, -uv_offset.y));
-            float4 src_t = SAMPLE_TEXTURE2D(_MainTex,sampler_MainTex, i.texcoord + float2(0, +uv_offset.y));
-            float4 src = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord);
-            float4 grad = abs(src_r - src_l) + abs(src_b - src_t);
-            float intens = saturate(0.333 * (grad.x + grad.y + grad.z));
-            float d = _EdgePower * intens + 1;
-            return ColorMod(src, d);
-        }
-
-        struct v2f_wobb {
-            float2 texcoord_Main : TEXCOORD0;
-            float2 texcoord_Wobb : TEXCOORD1;
-            float4 vertex : SV_POSITION;
-        };
-
-        v2f_wobb VertWobb(AttributesDefault v) {
-            float aspect = _ScreenParams.x / _ScreenParams.y;
-
-            v2f_wobb o;
-            o.vertex = float4(v.vertex.xy, 0.0, 1.0);                     
-            o.texcoord_Main = TransformTriangleVertexToUV(v.vertex.xy);
-            #if UNITY_UV_STARTS_AT_TOP
-                o.texcoord_Main = o.texcoord_Main * float2(1.0, -1.0) + float2(0.0, 1.0);
-            #endif
-            
-            o.texcoord_Wobb = o.texcoord_Main * float2(aspect, 1) * _WobbScale;
-            return o;
-        }
-
-        float4 FragWobbb(v2f_wobb i) : SV_Target {
-            // float4 mainColor = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord_Main);
-            float2 wobb = SAMPLE_TEXTURE2D(_WobbTex, sampler_WobbTex, i.texcoord_Wobb).wy * 2 - 1;
-            float4 src = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord_Main + wobb * _WobbPower);
-            return src;
-        }
-
+        }                
     ENDHLSL
 
     SubShader
@@ -84,31 +22,91 @@
         Pass
         {
             HLSLPROGRAM
-     
+                TEXTURE2D_SAMPLER2D(_WobbTex, sampler_WobbTex);
+                struct v2f_wobb {
+                    float2 texcoord_Main : TEXCOORD0;
+                    float2 texcoord_Wobb : TEXCOORD1;
+                    float4 vertex : SV_POSITION;
+                };  
 
+                v2f_wobb VertWobb(AttributesDefault v) {
+                    float aspect = _ScreenParams.x / _ScreenParams.y;
+                    v2f_wobb o;
+                    o.vertex = float4(v.vertex.xy, 0.0, 1.0);                     
+                    o.texcoord_Main = TransformTriangleVertexToUV(v.vertex.xy);
+                    #if UNITY_UV_STARTS_AT_TOP
+                        o.texcoord_Main = o.texcoord_Main * float2(1.0, -1.0) + float2(0.0, 1.0);
+                    #endif                    
+                    o.texcoord_Wobb = o.texcoord_Main * float2(aspect, 1) * _WobbScale;
+                    return o;
+                }
+               float4 FragWobbb(v2f_wobb i) : SV_Target {
+                    // float4 mainColor = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord_Main);
+                    float2 wobb = SAMPLE_TEXTURE2D(_WobbTex, sampler_WobbTex, i.texcoord_Wobb).wy * 2 - 1;
+                    float4 src = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord_Main + wobb * _WobbPower);
+                    return src;
+                }
                 #pragma vertex VertWobb
                 #pragma fragment FragWobbb
-
             ENDHLSL
         }
         // Edge Darkening
         Pass
         {
-            HLSLPROGRAM
-
+            HLSLPROGRAM                                
+                float4 FragEdgeDarkening(VaryingsDefault i): SV_Target {
+                    float2 uv_offset = _EdgeSize * _MainTex_TexelSize.xy;
+                    float4 src_l = SAMPLE_TEXTURE2D(_MainTex,sampler_MainTex, i.texcoord + float2(-uv_offset.x, 0));
+                    float4 src_r = SAMPLE_TEXTURE2D(_MainTex,sampler_MainTex, i.texcoord + float2(+uv_offset.x, 0));
+                    float4 src_b = SAMPLE_TEXTURE2D(_MainTex,sampler_MainTex, i.texcoord + float2(0, -uv_offset.y));
+                    float4 src_t = SAMPLE_TEXTURE2D(_MainTex,sampler_MainTex, i.texcoord + float2(0, +uv_offset.y));
+                    float4 src = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord);
+                    float4 grad = abs(src_r - src_l) + abs(src_b - src_t);
+                    float intens = saturate(0.333 * (grad.x + grad.y + grad.z));
+                    float d = _EdgePower * intens + 1;
+                    return ColorMod(src, d);
+                }
                 #pragma vertex VertDefault
                 #pragma fragment FragEdgeDarkening
             ENDHLSL
         }
 
-        // Paper Layer
-        // Pass 
-        // {
-        //     HLSLPROGRAM
-        //         #pragma vertex VertDefault
-        //         #pragma fragment Frag
-        //     ENDHLSL
-        // }
+        // // Paper Layer
+        Pass 
+        {
+            HLSLPROGRAM
+                uniform float _PaperScale;
+                uniform float _PaperPower;
+                TEXTURE2D_SAMPLER2D(_PaperTex, sampler_PaperTex);
+
+                struct v2f_paper {
+				    float2 texcoord_Main : TEXCOORD0;
+				    float2 texcoord_Paper : TEXCOORD1;
+				    float4 vertex : SV_POSITION;
+			    };
+                
+                v2f_paper VertPaper(AttributesDefault v) {              
+                    float aspect = _ScreenParams.x / _ScreenParams.y;
+                    v2f_paper o;
+                    o.vertex = float4(v.vertex.xy, 0.0, 1.0);                    
+                    o.texcoord_Main = TransformTriangleVertexToUV(v.vertex.xy);
+                    #if UNITY_UV_STARTS_AT_TOP
+                        o.texcoord_Main = o.texcoord_Main * float2(1.0, -1.0) + float2(0.0, 1.0);
+                    #endif                  
+                    o.texcoord_Paper = o.texcoord_Main * float2(aspect, 1) * _PaperScale;
+                    return o;
+			    }
+			
+                float4 FragPaper(v2f_paper i) : SV_Target {
+                    float4 src = SAMPLE_TEXTURE2D(_MainTex,sampler_MainTex, i.texcoord_Main);
+                    float paper = SAMPLE_TEXTURE2D(_PaperTex,sampler_PaperTex, i.texcoord_Paper).x;
+                    float d = _PaperPower * (paper - 0.5) + 1;
+                    return ColorMod(src, d);
+                }
+                #pragma vertex VertPaper
+                #pragma fragment FragPaper
+            ENDHLSL
+        }
     }
 }
 	
--- TestProjects/LWGraphicsTest/Assets/Scripts/WaterColors.cs
+++ TestProjects/LWGraphicsTest/Assets/Scripts/WaterColors.cs
@@ -12,21 +12,28 @@ namespace WaterColors
     public Texture paperTex;
     public float paperScale = 1f;
     public float paperPower = 1f;
+
   }
 
 
-  // [Serializable]
-  // public sealed class PaperParameter : ParameterOverride<PaperData>
-  // {
-  //   public PaperParameter()
-  //   {
-  //   }
+  [Serializable]
+  public sealed class PaperParameter : ParameterOverride<PaperData>
+  {
+    public PaperParameter()
+    {
+      value = null;
+    }
 
-  //   public override void Interp(PaperData from, PaperData to, float t)
-  //   {
-  //     // Do nothing
-  //   }
-  // }
+    public PaperParameter(PaperData value)
+    {
+      this.value = value;
+    }
+
+    public static bool IsValid(PaperParameter parameter)
+    {
+      return parameter != null && parameter.value != null && parameter.value.paperTex != null;
+    }
+  }
 
 
   [Serializable]
@@ -38,6 +45,9 @@ namespace WaterColors
     public FloatParameter wobbPower = new FloatParameter { value = 0.01f };
     public FloatParameter edgeSize = new FloatParameter { value = 1f };
     public FloatParameter edgePower = new FloatParameter { value = 3f };
+    public PaperParameter paperParameter1 = new PaperParameter();
+    public PaperParameter paperParameter2 = new PaperParameter();
+    public PaperParameter paperParameter3 = new PaperParameter();
     // public PaperParameter[] paperDataset;
   }
 
@@ -47,6 +57,8 @@ namespace WaterColors
     private const int PASS_WOBB = 0;
     private const int PASS_EDGE = 1;
     private const int PASS_PAPER = 2;
+    private PaperData[] _paperData = new PaperData[3];
+    private int _paperCount = 0;
 
     public override DepthTextureMode GetCameraFlags()
     {
@@ -64,11 +76,30 @@ namespace WaterColors
       sheet.properties.SetFloat("_WobbPower", settings.wobbPower);
       sheet.properties.SetFloat("_EdgeSize", settings.edgeSize);
       sheet.properties.SetFloat("_EdgePower", settings.edgePower);
-      var rt0 = RenderTexture.GetTemporary(context.screenWidth, context.screenHeight, 0, RenderTextureFormat.ARGB64);
-      var rt1 = RenderTexture.GetTemporary(context.screenWidth, context.screenHeight, 0, RenderTextureFormat.ARGB64);
+      var rt0 = RenderTexture.GetTemporary(context.screenWidth, context.screenHeight, 0, context.sourceFormat);
+      var rt1 = RenderTexture.GetTemporary(context.screenWidth, context.screenHeight, 0, context.sourceFormat);
 
-      context.command.BlitFullscreenTriangle(context.source, rt0, sheet, PASS_WOBB);
+      context.command.BlitFullscreenTriangle(context.source, rt1, sheet, PASS_WOBB);
       Swap(ref rt0, ref rt1);
+      UpdatePaperData();
+      if (_paperCount > 0)
+      {
+        context.command.BlitFullscreenTriangle(rt0, rt1, sheet, PASS_EDGE);
+        Swap(ref rt0, ref rt1);
+        for (var i = 0; i < _paperCount; i++)
+        {
+          var d = _paperData[i];
+          sheet.properties.SetFloat("_PaperPower", d.paperPower);
+          sheet.properties.SetFloat("_PaperScale", d.paperScale);
+          sheet.properties.SetTexture("_PaperTex", d.paperTex);
+          context.command.BlitFullscreenTriangle(rt0, (i == (_paperCount - 1) ? context.destination : rt1), sheet, PASS_PAPER);
+          Swap(ref rt0, ref rt1);
+        }
+      }
+      else
+      {
+        context.command.BlitFullscreenTriangle(rt0, context.destination, sheet, PASS_EDGE);
+      }
       // if (0 > 0)
       // {
       // context.command.BlitFullscreenTriangle(rt0, rt1, sheet, PASS_EDGE);
@@ -83,7 +114,6 @@ namespace WaterColors
       // }
       // else
       // {
-      context.command.BlitFullscreenTriangle(rt1, context.destination, sheet, PASS_EDGE);
       // }
       RenderTexture.ReleaseTemporary(rt0);
       RenderTexture.ReleaseTemporary(rt1);
@@ -97,103 +127,26 @@ namespace WaterColors
       src = dst;
       dst = tmp;
     }
+
+    private void UpdatePaperData()
+    {
+      _paperCount = 0;
+      _paperCount += PaperParameter.IsValid(settings.paperParameter1) ? 1 : 0;
+      _paperCount += PaperParameter.IsValid(settings.paperParameter2) ? 1 : 0;
+      _paperCount += PaperParameter.IsValid(settings.paperParameter3) ? 1 : 0;
+      var i = 0;
+      if (PaperParameter.IsValid(settings.paperParameter1))
+      {
+        _paperData[i++] = settings.paperParameter1.value;
+      }
+      if (PaperParameter.IsValid(settings.paperParameter2))
+      {
+        _paperData[i++] = settings.paperParameter2.value;
+      }
+      if (PaperParameter.IsValid(settings.paperParameter3))
+      {
+        _paperData[i++] = settings.paperParameter3.value;
+      }
+    }
   }
-
-  // // namespace WaterColorFilterSystem
-  // // {
-
-  // //   [ExecuteInEditMode]
-  // //   public class WaterColorFilter : MonoBehaviour
-  // //   {
-  // //     public const int PASS_WOBB = 0;
-  // //     public const int PASS_EDGE = 1;
-  // //     public const int PASS_PAPER = 2;
-
-  // //     public const string PROP_WOBB_TEX = "_WobbTex";
-  // //     public const string PROP_WOBB_TEX_SCALE = "_WobbScale";
-  // //     public const string PROP_WOBB_POWER = "_WobbPower";
-  // //     public const string PROP_EDGE_SIZE = "_EdgeSize";
-  // //     public const string PROP_EDGE_POWER = "_EdgePower";
-  // //     public const string PROP_PAPER_TEX = "_PaperTex";
-  // //     public const string PROP_PAPER_SCALE = "_PaperScale";
-  // //     public const string PROP_PAPER_POWER = "_PaperPower";
-
-  // //     public Shader filter;
-
-  // //     public Texture wobbTex;
-  // //     public float wobbScale = 1f;
-  // //     public float wobbPower = 0.01f;
-  // //     public float edgeSize = 1f;
-  // //     public float edgePower = 3f;
-  // //     public PaperData[] paperDataset;
-
-  // //     Material _filterMat;
-
-  // //     void OnEnable()
-  // //     {
-  // //       _filterMat = new Material(filter);
-  // //     }
-  // //     void OnDisable()
-  // //     {
-  // //       DestroyImmediate(_filterMat);
-  // //     }
-  // //     void OnRenderImage(RenderTexture src, RenderTexture dst)
-  // //     {
-  // //       _filterMat.SetTexture(PROP_WOBB_TEX, wobbTex);
-  // //       _filterMat.SetFloat(PROP_WOBB_TEX_SCALE, wobbScale);
-  // //       _filterMat.SetFloat(PROP_WOBB_POWER, wobbPower);
-  // //       _filterMat.SetFloat(PROP_EDGE_SIZE, edgeSize);
-  // //       _filterMat.SetFloat(PROP_EDGE_POWER, edgePower);
-
-  // //       var rt0 = RenderTexture.GetTemporary(src.width, src.height, 0, RenderTextureFormat.ARGB32);
-  // //       var rt1 = RenderTexture.GetTemporary(src.width, src.height, 0, RenderTextureFormat.ARGB32);
-  // //       var nPapers = paperDataset.Length;
-
-  // //       Graphics.Blit(src, rt1, _filterMat, PASS_WOBB);
-  // //       Swap(ref rt0, ref rt1);
-
-  // //       if (nPapers > 0)
-  // //       {
-  // //         Graphics.Blit(rt0, rt1, _filterMat, PASS_EDGE);
-  // //         Swap(ref rt0, ref rt1);
-
-  // //         for (var i = 0; i < nPapers; i++)
-  // //         {
-  // //           paperDataset[i].SetProps(_filterMat);
-  // //           Graphics.Blit(rt0, (i == (nPapers - 1) ? dst : rt1), _filterMat, PASS_PAPER);
-  // //           Swap(ref rt0, ref rt1);
-  // //         }
-  // //       }
-  // //       else
-  // //       {
-  // //         Graphics.Blit(rt0, dst, _filterMat, PASS_EDGE);
-  // //       }
-
-  // //       RenderTexture.ReleaseTemporary(rt0);
-  // //       RenderTexture.ReleaseTemporary(rt1);
-  // //     }
-
-  // //     void Swap(ref RenderTexture src, ref RenderTexture dst)
-  // //     {
-  // //       var tmp = src;
-  // //       src = dst;
-  // //       dst = tmp;
-  // //     }
-
-  // //     [System.Serializable]
-  // //     public class PaperData
-  // //     {
-  // //       public Texture paperTex;
-  // //       public float paperScale = 1f;
-  // //       public float paperPower = 1f;
-
-  // //       public void SetProps(Material mat)
-  // //       {
-  // //         mat.SetTexture(PROP_PAPER_TEX, paperTex);
-  // //         mat.SetFloat(PROP_PAPER_SCALE, paperScale);
-  // //         mat.SetFloat(PROP_PAPER_POWER, paperPower);
-  // //       }
-  // //     }
-  // //   }
-  // // }
 }
